21a9a43aa9f66d2c168a65b0ea0a0f01d665fbdc
Victor <victorravain@hotmail.fr>
2025-05-19 17:03:00 +0200

    Separate servo control and xbee data Get current position and command from shared_data

--- platformIO/src/main.cpp
+++ platformIO/src/main.cpp
@@ -12,10 +12,13 @@
 #define LED_PIN 25 // Broche LED pour Raspberry Pi Pico
 
 servoControl boat;
+xbeeImpl xbee;
 
 // Déclaration des tâches existantes
 void TaskBlink(void *pvParameters);
 void exampleTask(void *pvParameters);
+void xbeeTask(void *pvParameters);
+void controlTask(void *pvParameters);
 void pathFinding(void *pvParameters);
 // Nouvelle tâche pour les capteurs
 void sensorTask(void *pvParameters);
@@ -32,26 +35,34 @@ CMPS12 cmps12(I2C0Instance, 0x60);
 
 void setup()
 {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    // Création des tâches FreeRTOS
-    // xTaskCreate(
-    //     TaskBlink,        // Fonction de la tâche
-    //     "LED Task",       // Nom de la tâche
-    //     1024,             // Taille de la pile
-    //     NULL,             // Paramètre
-    //     1,                // Priorité
-    //     NULL              // Handle de tâche
-    // );
+  xTaskCreate(
+      TaskBlink,  // Fonction de la tâche
+      "LED Task", // Nom de la tâche
+      1024,       // Taille de la pile
+      NULL,       // Paramètre
+      1,          // Priorité
+      NULL        // Handle de tâche (inutile ici)
+  );
 
-    xTaskCreate(
-        TaskBlink,  // Fonction de la tâche
-        "LED Task", // Nom de la tâche
-        1024,       // Taille de la pile
-        NULL,       // Paramètre
-        1,          // Priorité
-        NULL        // Handle de tâche (inutile ici)
-    );
+  xTaskCreate(
+    xbeeTask,  // Fonction de la tâche
+    "Xbee data transceiver", // Nom de la tâche
+    1024,       // Taille de la pile
+    NULL,       // Paramètre
+    1,          // Priorité
+    NULL        // Handle de tâche (inutile ici)
+  );
+
+  xTaskCreate(
+    controlTask,  // Fonction de la tâche
+    "control task", // Nom de la tâche
+    1024,       // Taille de la pile
+    NULL,       // Paramètre
+    1,          // Priorité
+    NULL        // Handle de tâche (inutile ici)
+  );
 
   xTaskCreate(
     pathFinding,        // Fonction de la tâche
@@ -60,7 +71,7 @@ void setup()
     NULL,             // Paramètre
     1,                // Priorité
     NULL              // Handle de tâche (inutile ici)
-);
+  );
 
     // Démarrer le planificateur FreeRTOS (optionnel sur Arduino)
     // vTaskStartScheduler();
@@ -83,6 +94,21 @@ void TaskBlink(void *pvParameters)
         digitalWrite(2, LOW);
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
+}
+
+void xbeeTask(void *pvParameters) {
+  while (1)
+  {
+    xbee.Read();
+    xbee.getValue();
+  }
+}
+
+void controlTask(void *pvParameters) {
+  while (1)
+  {
+    boat.servo_control(xbee);
+  }
 }
 
 // Nouvelle tâche pour la gestion des capteurs
--- platformIO/src/xbeeImpl.cpp
+++ platformIO/src/xbeeImpl.cpp
@@ -6,6 +6,9 @@ xbeeImpl::xbeeImpl()
     pinMode(XBee_rssi_pin, INPUT);
     pinMode(XBee_dout_pin, INPUT_PULLUP);
     pinMode(XBee_reset_pin, OUTPUT);
+    pinMode(XBee_din_pin, OUTPUT);
+    pinMode(rtk_rx_pin, INPUT);
+    pinMode(rtk_tx_pin, OUTPUT);
     digitalWrite(XBee_reset_pin, LOW);
     delay(10);
     digitalWrite(XBee_reset_pin, HIGH);
@@ -13,25 +16,79 @@ xbeeImpl::xbeeImpl()
     Serial1.setRX(XBee_dout_pin);
     Serial1.setTX(XBee_din_pin);
     Serial1.begin(9600, SERIAL_8N1);
+
+    Serial2.setRX(rtk_rx_pin);
+    Serial2.setTX(rtk_tx_pin);
+    Serial2.begin(38400, SERIAL_8N1);
 }
 
-String xbeeImpl::Read()
+void xbeeImpl::Read()
 {
     if (Serial1.available())
     {
-        String receivedMessage = "";
-
-        while (Serial1.available())
+        receivedMessage = "";
+        char c = ' ';
+        while (c != '|')
         {
-            char c = Serial1.read();
-            receivedMessage += c;
+            if (Serial1.available())
+            {
+                c = Serial1.read();
+                if (c != '|')
+                {
+                    receivedMessage += c;
+                }
+            }
         }
-        return receivedMessage;
+        Serial.println(receivedMessage);
+        receivedMessage.trim();
+        Serial2.write((const uint8_t *)receivedMessage.c_str(), receivedMessage.length());
     }
-    return "";
+
+    delay(100);
 }
 
-void xbeeImpl::Send(int currentTension, int currentAngle, int targetAngle, int servoAnglePosition, Stream &output)
+void xbeeImpl::getValue()
+{
+    // Find the position of the ':'
+    int separatorIndex = receivedMessage.indexOf(':');
+
+    if (separatorIndex != -1)
+    {
+        // Extract the key and value
+        String key = receivedMessage.substring(0, separatorIndex);
+        String value = receivedMessage.substring(separatorIndex + 1);
+
+        // Convert value to integer or float depending on the key
+        if (key == "ki")
+        {
+            Ki = value.toFloat();
+            Serial.print("ki value: ");
+            Serial.println(Ki);
+        }
+        else if (key == "kp")
+        {
+            Kp = value.toFloat();
+            Serial.print("kp value: ");
+            Serial.println(Kp);
+        }
+        else if (key == "rtk") {
+            rtk = value;
+            Serial.println(rtk);
+            Serial2.print(rtk);
+            Serial.println("rtk value sended to GPS");
+
+        }
+        else {
+            Serial.println("Invalid key. Expected 'kp', 'ki' or 'rtk.");
+        }
+    }
+    else
+    {
+        Serial.println("Invalid format. Expected 'key:value'.");
+    }
+}
+
+void xbeeImpl::Send(int currentTension, int currentAngle, int targetAngle, int servoAnglePosition) const
 {
     static int previous_curr = -1;
     static int previous_target = -1;
@@ -40,29 +97,29 @@ void xbeeImpl::Send(int currentTension, int currentAngle, int targetAngle, int s
 
     if (currentAngle != previous_curr)
     {
-        output.print("current angle:");
-        output.println(currentAngle);
+        Serial1.print("current angle:");
+        Serial1.println(currentAngle);
         previous_curr = currentAngle;
     }
 
     if (currentTension != previous_tension)
     {
-        output.print("current tension:");
-        output.println(currentTension);
+        Serial1.print("current tension:");
+        Serial1.println(currentTension);
         previous_tension = currentTension;
     }
 
     if (targetAngle != previous_target)
     {
-        output.print("target angle:");
-        output.println(targetAngle);
+        Serial1.print("target angle:");
+        Serial1.println(targetAngle);
         previous_target = targetAngle;
     }
 
     if (servoAnglePosition != previous_serv)
     {
-        output.print("servo angle position:");
-        output.println(servoAnglePosition);
+        Serial1.print("servo angle position:");
+        Serial1.println(servoAnglePosition);
         previous_serv = servoAnglePosition;
     }
 }
